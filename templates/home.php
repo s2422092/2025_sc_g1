7fec495408776d52e2b28b9a66bbbc2171cd4029
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-08-29 17:08:48 +0900

    最低限のディブを作成した

---
+++ templates/home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ホーム</title>
+    <link rel="stylesheet" href=>
+</head>
+
+<body>
+    <header class="main-header">
+        <h1>ホーム</h1>
+        <nav class="right">
+            <ul>
+                <li><a href="#">フォロー</a></li>
+                <li><a href="#">おすすめ</a></li>
+            </ul>
+        </nav>        
+    </header>
+
+    <div class="main-content">
+        <div>
+            <h2>ユーザー情報</h2>
+        </div>
+
+        <div>
+            <h2>コメント欄</h2>
+        </div>
+
+        <div>
+            <h2>いいね表示</h2>
+        </div>
+
+        <div>
+            <h2>フォロー</h2>
+        </div>
+
+    </div>
+
+    <nav class="main-nav-under">
+        <ul>
+            <li><a href="#">ホーム</a></li>
+            <li><a href="#">投稿</a></li>
+            <li><a href="#">マイページ</a></li>
+        </ul>
+    </nav>
+
+    <div class="main">
+        
+    </div>
+</body>
+</html>
