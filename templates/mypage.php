7140a8f09739472d6f6c2cca8dcfd16d71bcaafe
sugawara yui <s2222049@stu.musashino-u.ac.jp>
2025-08-30 17:07:01 +0900

    css対応とフォローフォロワー数の表示

--- templates/mypage.php
+++ templates/mypage.php
@@ -64,35 +64,62 @@ pg_close($dbconn);
     <title>夏合宿</title>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" />
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
-    <!--link href="mythologymap.css" rel="stylesheet" type="text/css" media="all" /-->
+    <link href="mypage.css" rel="stylesheet" type="text/css" media="all">
 </head>
 <body>
     <header>
         <h1 class="test">mypage</h1>
     </header>
     <div class="userdata">
-        <p><strong><img src="<?php echo htmlspecialchars($userprofile_image); ?>" alt="NO DATA"></p><!--アイコン-->
-        <p><strong><?php echo htmlspecialchars($username); ?></strong></p><!--ユーザー名-->
-        <p><strong><?php echo htmlspecialchars($userstart_date); ?></strong></p><!--登録日-->
-        <p><strong>身長：<?php echo htmlspecialchars($userheight); ?></strong></p> <!--身長-->
-        <p><strong>体格：<?php echo htmlspecialchars($userframe); ?></strong></p> <!--体格-->
+        <section class="profile">
+            <div class="profile-header">
+                <p><strong><img src="<?php echo htmlspecialchars($userprofile_image); ?>" alt="プロフィールアイコン" class="profile-icon"></p><!--アイコン-->
+                <div>
+                    <p><strong><?php echo htmlspecialchars($username); ?></strong></p><!--ユーザー名-->
+                    <p class="detail-info"><sub><?php echo htmlspecialchars($userstart_date); ?></sub></p><!--登録日-->
+                </div>
+            </div>
+            <div style="display: flex; margin-top: 15px;">
+                <div class="box3">
+                    <p><strong>身長：<?php echo htmlspecialchars($userheight); ?></strong></p> <!--身長-->
+                </div>
+                <div class="box4">
+                    <p><strong>体格：<?php echo htmlspecialchars($userframe); ?></strong></p> <!--体格-->
+                </div>
+            </div>
+        </section>
+        <div class="follow-stats">
+            <div class="follow-item">
+                <?php
+                $f_sql = "SELECT count(*) FROM user_follow WHERE follower_uid = $userId;";
+                $r = pg_query($f_sql) or die('Error: ' . pg_last_error());
+                $row=pg_fetch_row($r);
+                $count = $row[0];
+                echo"<p class=\"follow-count\">" . $count . "</p>";//フォロー数計算
+                ?>
+                <p class="follow-label">フォロー</p>
+                <?php
+                $fe_sql = "SELECT count(*) FROM user_follow WHERE followee_uid = $userId;";
+                $r_fe = pg_query($fe_sql) or die('Error: ' . pg_last_error());
+                $rowf=pg_fetch_row($r_fe);
+                $count_fe = $rowf[0];
+                echo"<p class=\"follow-count\">" . $count_fe . "</p>";//フォロワー数計算
+                ?>
+                <p class="follow-label">フォロワー</p>
+            </div>
+            <details close>
+                <summary>フォロー一覧</summary>
+                    <p><img src="<?php echo htmlspecialchars($followee_profileImage); ?>" alt="NO DATA"></p><!--フォロー相手のアイコン-->
+                    <p><?php echo htmlspecialchars($followee_name); ?></p><!--フォロー相手の名前-->
+            </details>
         </div>
-    </div>
-    <div class="post_history">
-        <details>
-            <summary>投稿履歴</summary>
-            <p><img src="<?php echo htmlspecialchars($post_image); ?>" alt="NO DATA"></p><!--アイコン-->
-            <p><?php echo htmlspecialchars($uname); ?></p><!--ユーザー名-->
-            <p><?php echo htmlspecialchars($post_text); ?></p><!--投稿内容-->
-            <p><?php echo htmlspecialchars($post_date); ?></p><!--投稿日時-->
-        </details>    
-    </div>
-    <div class="follow_list">
-        <details>
-            <summary>フォロー一覧</summary>
-            <p><img src="<?php echo htmlspecialchars($followee_profileImage); ?>" alt="NO DATA"></p><!--フォロー相手のアイコン-->
-            <p><?php echo htmlspecialchars($followee_name); ?></p><!--フォロー相手の名前-->
-        </details>    
+        <div class="post_history">
+            <details>
+                <summary>投稿履歴</summary>
+                <p><img src="<?php echo htmlspecialchars($post_image); ?>" alt="NO DATA"></p><!--投稿画像-->
+            </details>    
+        </div>
+    </div>  
 </div>
     
 </body>
